cd704ffa7d6111f348b1782f5c1c68bf50abf140
Justin Nguyen <justinn123123123@gmail.com>
2023-02-19 14:45:26 -0500

    changed the base condition

--- Recursion_Lab/src/ArraySum.java
+++ Recursion_Lab/src/ArraySum.java
@@ -1,9 +1,9 @@
 public class ArraySum {
     public int sumOfArray(Integer[] a, int index){
         if(index==0)
-            return 0;
+            return a[0];
         else{
-            return a[index-1]+sumOfArray(a, index-1);
+            return a[index]+sumOfArray(a, index-1);
         }
     }
 }
