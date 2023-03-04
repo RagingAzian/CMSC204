7ff7e0d2f80ce010dccf78dbefb114dad3fe96c2
Justin Nguyen <justinn123123123@gmail.com>
2023-03-03 23:20:30 -0500

    killing my braincells

--- Assignment3/src/SortedDoubleLinkedList.java
+++ Assignment3/src/SortedDoubleLinkedList.java
@@ -1,42 +1,42 @@
 import java.util.*;
 
-public class SortedDoubleLinkedList<T> extends BasicDoubleLinkedList<T>{
+public class SortedDoubleLinkedList<T> extends BasicDoubleLinkedList<T> {
     private Comparator<T> comparator;
-    public SortedDoubleLinkedList(Comparator<T> comparator){
+
+    public SortedDoubleLinkedList(Comparator<T> comparator) {
         this.comparator = comparator;
     }
 
-    public void add (T data){
-        if(size==0 || comparator.compare(head.getItem(), data)>=0){
+    public void add(T data) {
+        if (size == 0 || comparator.compare(head.getItem(), data) >= 0) {
             super.addToFront(data);
             return;
         }
-        if(comparator.compare(tail.getItem(),data)<=0){
+        if (comparator.compare(tail.getItem(), data) <= 0) {
             super.addToEnd(data);
             return;
         }
-        int prev = -1;
-        int current;
 
         Node currentNode = head;
         Node newNode;
 
-        while(true){
-            current = comparator.compare(currentNode.getItem(),data);
-            if(prev < 0 && current >= 0 ){
-				newNode = new Node(data, currentNode.getPrev(), currentNode);
-				newNode.setPrev(currentNode.getPrev());
-				newNode.setNext(currentNode);
+        while (true) {
+            int compareResult = comparator.compare(currentNode.getItem(), data);
+            if (compareResult >= 0) {
+                newNode = new Node(data, currentNode.getPrev(), currentNode);
                 currentNode.getPrev().setNext(newNode);
                 currentNode.setPrev(newNode);
-				size++;
-				break;
+                size++;
+                break;
             }
-            prev = current;
+            if (currentNode == tail) {
+                super.addToEnd(data);
+                break;
+            }
             currentNode = currentNode.getNext();
         }
-        return;
     }
+
     public void addToEnd(T data) {
 		throw new UnsupportedOperationException("Invalid operation for sorted list");
 	}
