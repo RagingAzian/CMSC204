9999d85b4f6699f20d9a25522a17de1175d27be3
Justin Nguyen <justinn123123123@gmail.com>
2023-03-05 14:53:18 -0500

    killing my braincells but it actually worked :)

--- Assignment3/src/BasicDoubleLinkedList.java
+++ Assignment3/src/BasicDoubleLinkedList.java
@@ -1,233 +1,364 @@
-import java.util.*;
 
+/**
+ * This generic double-linked implements the Java's Iterable interface and relies on a head (reference to first element of the list) and tail (reference to the last element of the list). Both the head and the tail are set to null when the list is empty. Both point to the same element when there is only one element in the list. A node structure has three fields: data, next and the previous references. 
+ * @author Justin Nguyen
+ * @param <T>
+ */
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 public class BasicDoubleLinkedList<T> implements Iterable<T> {
-    protected int size;
-    protected Node head;
-    protected Node tail;
+	protected int size;
+	protected Node head;
+	protected Node tail;
 
-    public BasicDoubleLinkedList() {
-        size = 0;
-        head = tail = null;
-    }
+	/**
+	 * 
+	 */
+	public BasicDoubleLinkedList() {
+		size = 0;
+		head = tail = null;
+	}
 
-    public int getSize() {
-        return size;
-    }
+	/**
+	 * 
+	 * @return
+	 */
+	public int getSize() {
+		return size;
+	}
 
-    public void addToEnd(T data) {
-        if (tail == null) {
-            tail = new Node(data, null, null);
-            head = tail;
-        } else {
-            Node nextLast = new Node(data, tail, null);
-            tail.setNext(nextLast);
-            tail = nextLast;
-        }
-        size++;
-    }
+	/**
+	 * 
+	 * @param data
+	 */
+	public void addToEnd(T data) {
+		if (tail == null) {
+			tail = new Node(data, null, null);
+			head = tail;
+		} else {
+			Node nextLast = new Node(data, tail, null);
+			tail.setNext(nextLast);
+			tail = nextLast;
+		}
+		size++;
+	}
 
-    public void addToFront(T data) {
-        if (head == null) {
-            head = new Node(data, null, null);
-            tail = head;
-        } else {
-            Node nextFirst = new Node(data, null, head);
-            head.setPrev(nextFirst);
-            head = nextFirst;
-        }
-        size++;
-    }
+	/**
+	 * 
+	 * @param data
+	 */
+	public void addToFront(T data) {
+		if (head == null) {
+			head = new Node(data, null, null);
+			tail = head;
+		} else {
+			Node nextFirst = new Node(data, null, head);
+			head.setPrev(nextFirst);
+			head = nextFirst;
+		}
+		size++;
+	}
 
-    public T getFirst() {
-        if (head != null)
-            return head.getItem();
-        return null;
-    }
+	/**
+	 * 
+	 * @return
+	 */
+	public T getFirst() {
+		if (head != null)
+			return head.getItem();
+		return null;
+	}
 
-    public T getLast() {
-        if (tail != null)
-            return tail.getItem();
-        return null;
-    }
+	/**
+	 * 
+	 * @return
+	 */
+	public T getLast() {
+		if (tail != null)
+			return tail.getItem();
+		return null;
+	}
 
-    public ListIterator<T> iterator() throws UnsupportedOperationException, NoSuchElementException {
-        return new DoubleLinkedListIterator();
-    }
+	/**
+	 * 
+	 */
+	public ListIterator<T> iterator() throws UnsupportedOperationException, NoSuchElementException {
+		return new DoubleLinkedListIterator();
+	}
 
-    public Node remove(T targetData, Comparator<T> comparator) {
-        Node current = head;
+	/**
+	 * 
+	 * @param targetData
+	 * @param comparator
+	 * @return
+	 */
+	public Node remove(T targetData, Comparator<T> comparator) {
+		Node current = head;
 
-        if (size == 0) {
-            return current;
-        }
-        if (size == 1) {
-            if (comparator.compare(targetData, head.getItem()) == 0) {
-                head = tail = null;
-                size--;
-            }
-            return current;
-        }
+		if (size == 0) {
+			return current;
+		}
+		if (size == 1) {
+			if (comparator.compare(targetData, head.getItem()) == 0) {
+				head = tail = null;
+				size--;
+			}
+			return current;
+		}
 
-        while (current != null) {
-            if(comparator.compare(targetData,current.getItem())!=0)
-                current = current.getNext();
-            else if (current.equals(head)) {
-                head = current.getNext();
-                current.getNext().setPrev(null);
-                size--;
-                break;
-            } else if (current.equals(tail)) {
-                tail = current.getPrev();
-                current.getPrev().setNext(null);
-                size--;
-                break;
-            } else {
-                current.getPrev().setNext(current.getNext());
-                current.getNext().setPrev(current.getPrev());
-                size--;
-                break;
-            }
-        }
-        return current;
-    }
+		while (current != null) {
+			if (comparator.compare(targetData, current.getItem()) != 0)
+				current = current.getNext();
+			else if (current.equals(head)) {
+				head = current.getNext();
+				current.getNext().setPrev(null);
+				size--;
+				break;
+			} else if (current.equals(tail)) {
+				tail = current.getPrev();
+				current.getPrev().setNext(null);
+				size--;
+				break;
+			} else {
+				current.getPrev().setNext(current.getNext());
+				current.getNext().setPrev(current.getPrev());
+				size--;
+				break;
+			}
+		}
+		return current;
+	}
 
-    public T retrieveFirstElement(){
-        if (head == null)
-            return null;
-        T t = head.getItem();
-        if(size ==1){
-            head = tail = null;
-            return t;
-        }
-        head = head.getNext();
-        head.setPrev(null);
-        return t;
-    }
+	/**
+	 * 
+	 * @return
+	 */
+	public T retrieveFirstElement() {
+		if (head == null)
+			return null;
+		T t = head.getItem();
+		if (size == 1) {
+			head = tail = null;
+			return t;
+		}
+		head = head.getNext();
+		head.setPrev(null);
+		return t;
+	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	public T retrieveLastElement() {
+		if (tail == null)
+			return null;
+		T t = tail.getItem();
+		if (size == 1) {
+			head = null;
+			tail = null;
+			return t;
+		}
+		tail = tail.getPrev();
+		tail.setNext(null);
+		size--;
+		return t;
+	}
 
-    public T retrieveLastElement(){
-        if(tail==null)
-            return null;
-        T t = tail.getItem();
-        if(size==1){
-            head = null;
-            tail = null;
-            return t;
-        }
-        tail = tail.getPrev();
-        tail.setNext(null);
-        size--;
-        return t;
-    }
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<T> toArrayList() {
+		ArrayList<T> list = new ArrayList<T>();
+		DoubleLinkedListIterator iterator = (DoubleLinkedListIterator) iterator();
+		while (iterator.hasNext())
+			list.add(iterator.next());
+		return list;
+	}
 
-    public ArrayList<T> toArrayList() {
-        ArrayList<T> list = new ArrayList<T>();
-        DoubleLinkedListIterator iterator = (DoubleLinkedListIterator) iterator();
-        while (iterator.hasNext()) 
-            list.add(iterator.next());
-        return list;
-    }
+	/**
+	 * Inner class to implement Nodes for list
+	 * 
+	 * @author Justin Nguyen
+	 *
+	 */
+	protected class Node {
+		private T data;
+		private Node prev, next;
 
-    protected class Node {
-        private T data;
-        private Node prev, next;
+		/**
+		 * 
+		 * @param data
+		 * @param prev
+		 * @param next
+		 */
+		Node(T data, Node prev, Node next) {
+			this.data = data;
+			this.prev = prev;
+			this.next = next;
+		}
 
-        Node(T data, Node prev, Node next) {
-            this.data = data;
-            this.prev = prev;
-            this.next = next;
-        }
+		/**
+		 * 
+		 */
+		Node() {
+			data = null;
+			prev = null;
+			next = null;
+		}
 
-        Node(){
-            data = null;
-            prev = null;
-            next = null;
-        }
+		/**
+		 * 
+		 * @param data
+		 */
+		Node(T data) {
+			this.data = data;
+			prev = null;
+			next = null;
+		}
 
-        Node(T data){
-            this.data = data;
-            prev = null;
-            next = null;
-        }
+		/**
+		 * 
+		 * @return
+		 */
+		public T getItem() {
+			return data;
+		}
 
-        public T getItem() {
-            return data;
-        }
+		/**
+		 * 
+		 * @return
+		 */
+		public Node getPrev() {
+			return prev;
+		}
 
-        public Node getPrev() {
-            return prev;
-        }
+		/**
+		 * 
+		 * @return
+		 */
+		public Node getNext() {
+			return next;
+		}
 
-        public Node getNext() {
-            return next;
-        }
+		/**
+		 * 
+		 * @param n
+		 */
+		public void setNext(Node n) {
+			next = n;
+		}
 
-        public void setNext(Node n) {
-            next = n;
-        }
+		/**
+		 * 
+		 * @param n
+		 */
+		public void setPrev(Node n) {
+			prev = n;
+		}
 
-        public void setPrev(Node n) {
-            prev = n;
-        }
+		/**
+		 * 
+		 * @param n
+		 * @return
+		 */
+		public boolean equals(Node n) {
+			return (next == n.getNext() && prev == n.getPrev() && data == n.getItem());
+		}
+	}
 
-        public boolean equals(Node n) {
-            return (next == n.getNext() && prev == n.getPrev() && data == n.getItem());
-        }
-    }
+	/**
+	 * Inner class for iterator for list
+	 * 
+	 * @author Justin Nguyen
+	 *
+	 */
+	protected class DoubleLinkedListIterator implements ListIterator<T> {
+		Node next, prev = next = null;
 
-    protected class DoubleLinkedListIterator implements ListIterator<T> {
-        Node next, prev = next = null;
+		/**
+		 * 
+		 */
+		DoubleLinkedListIterator() {
+			next = head;
+		}
 
-        DoubleLinkedListIterator() {
-            next = head;
-        }
+		/**
+		 * 
+		 */
+		public boolean hasNext() {
+			return next != null;
+		}
 
-        public boolean hasNext() {
-            return next!=null;
-        }
+		/**
+		 * 
+		 */
+		public boolean hasPrevious() {
+			return prev != null;
+		}
 
-        public boolean hasPrevious() {
-            return prev!=null;
-        }
+		/**
+		 * 
+		 */
+		public T next() throws NoSuchElementException {
+			if (!hasNext())
+				throw new NoSuchElementException();
+			T t = next.getItem();
+			prev = next;
+			next = next.next;
+			return t;
+		}
 
-        public T next() throws NoSuchElementException{
-            if(!hasNext())
-                throw new NoSuchElementException();
-            T t = next.getItem();
-            prev = next;
-            next = next.next;
-            return t;
-        }
+		/**
+		 * 
+		 */
+		public T previous() throws NoSuchElementException {
+			if (!hasPrevious()) {
+				throw new NoSuchElementException();
+			}
+			T t = prev.getItem();
+			next = prev;
+			prev = prev.prev;
+			return t;
+		}
 
-        public T previous() throws NoSuchElementException {
-            if (!hasPrevious()) {
-                throw new NoSuchElementException();
-            }
-            T t = prev.getItem();
-            next = prev;
-            prev = prev.prev;
-            return t;
-        }
-        
-        public int nextIndex() {
-            throw new UnsupportedOperationException();
-        }
+		/**
+		 * 
+		 */
+		public int nextIndex() {
+			throw new UnsupportedOperationException();
+		}
 
-        public int previousIndex() {
-            throw new UnsupportedOperationException();
-        }
+		/**
+		 * 
+		 */
+		public int previousIndex() {
+			throw new UnsupportedOperationException();
+		}
 
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
+		/**
+		 * 
+		 */
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
 
-        public void set(T t) {
-            throw new UnsupportedOperationException();
-        }
+		/**
+		 * 
+		 */
+		public void set(T t) {
+			throw new UnsupportedOperationException();
+		}
 
-        public void add(T t) {
-            throw new UnsupportedOperationException();
-        }
-    }
+		/**
+		 * 
+		 */
+		public void add(T t) {
+			throw new UnsupportedOperationException();
+		}
+	}
 }
--- Assignment3/src/SortedDoubleLinkedList.java
+++ Assignment3/src/SortedDoubleLinkedList.java
@@ -1,53 +1,79 @@
-import java.util.*;
+
+/**Class A generic sorted double linked list will be constructed using a provided Comparator to determine how the list is to be sorted.
+ * @author: Justin Nguyen
+ * @param <T>
+ */
+
+import java.util.Comparator;
+import java.util.ListIterator;
 
 public class SortedDoubleLinkedList<T> extends BasicDoubleLinkedList<T> {
-    private Comparator<T> comparator;
+	private Comparator<T> comparator;
 
-    public SortedDoubleLinkedList(Comparator<T> comparator) {
-        this.comparator = comparator;
-    }
+	public SortedDoubleLinkedList(Comparator<T> comparator) {
+		this.comparator = comparator;
+	}
 
-    public void add(T data) {
-        if (size == 0 || comparator.compare(head.getItem(), data) >= 0) {
-            super.addToFront(data);
-            return;
-        }
-        if (comparator.compare(tail.getItem(), data) <= 0) {
-            super.addToEnd(data);
-            return;
-        }
+	/**
+	 * 
+	 * @param data
+	 */
+	public void add(T data) {
+		if (size == 0 || comparator.compare(head.getItem(), data) >= 0) {
+			super.addToFront(data);
+			return;
+		}
+		if (comparator.compare(tail.getItem(), data) <= 0) {
+			super.addToEnd(data);
+			return;
+		}
 
-        Node currentNode = head;
-        Node newNode;
+		Node currentNode = head;
+		Node newNode;
 
-        while (true) {
-            int compareResult = comparator.compare(currentNode.getItem(), data);
-            if (compareResult >= 0) {
-                newNode = new Node(data, currentNode.getPrev(), currentNode);
-                currentNode.getPrev().setNext(newNode);
-                currentNode.setPrev(newNode);
-                size++;
-                break;
-            }
-            if (currentNode == tail) {
-                super.addToEnd(data);
-                break;
-            }
-            currentNode = currentNode.getNext();
-        }
-    }
+		while (true) {
+			int compareResult = comparator.compare(currentNode.getItem(), data);
+			if (compareResult >= 0) {
+				newNode = new Node(data, currentNode.getPrev(), currentNode);
+				currentNode.getPrev().setNext(newNode);
+				currentNode.setPrev(newNode);
+				size++;
+				break;
+			}
+			if (currentNode == tail) {
+				super.addToEnd(data);
+				break;
+			}
+			currentNode = currentNode.getNext();
+		}
+	}
 
-    public void addToEnd(T data) {
+	/**
+	 * 
+	 */
+	public void addToEnd(T data) {
 		throw new UnsupportedOperationException("Invalid operation for sorted list");
 	}
 
+	/**
+	 * 
+	 */
 	public void addToFront(T data) {
 		throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * 
+	 */
 	public ListIterator<T> iterator() {
 		return super.iterator();
 	}
+
+	/**
+	 * @param data
+	 * @param comparator
+	 * @return
+	 */
 
 	public Node remove(T data, Comparator<T> comparator) {
 		return (Node) super.remove(data, comparator);
