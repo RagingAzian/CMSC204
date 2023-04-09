d48d1873cd6d220f97be7f062e3b23c66867e2ce
Justin Nguyen <justinn123123123@gmail.com>
2023-04-09 12:15:03 -0400

    this did work

---
+++ Assignment5/src/MorseCodeConverter_GFA_STUDENT_Test.java
@@ -0,0 +1,59 @@
+ 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MorseCodeConverter_GFA_STUDENT_Test {
+	
+	@Test
+	public void testConvertToEnglishString() {	
+		String converter1 = MorseCodeConverter.convertToEnglish(".... . .-.. .-.. --- / .-- --- .-. .-.. -.. ");
+		assertEquals("hello world",converter1);
+	}
+	
+	/**
+	 * Testing for correct implementation of tree and traversal
+	 */
+	
+	@Test
+	public void testPrintTree() {	
+		
+		assertEquals("h s v i f u e l r a p w j  b d x n c k y t z g q m o", MorseCodeConverter.printTree());
+	}
+	
+	/**
+	 * Testing for correct conversion of all characters using key phrase to hit all letters
+	 */
+	
+	@Test
+	public void testConvertMorseStringToEnglishString() {	
+		
+		String converter1 = MorseCodeConverter.convertToEnglish(".. / .-.. .. -.- . / . .- - .. -. --. / .-. .. -.-. .");
+		assertEquals("i like eating rice", converter1);
+
+	}
+	@Test
+	public void testConvertMorseFileToEnglishString() {	
+		
+		/*Make sure howDoILoveThee.txt is in the src directory for this 
+		  test to pass
+		*/
+		File file = new File("C:/Users/justi/CMSC204/CMSC204/Assignment5/src/Vagabond.txt"); 
+		try {
+			assertEquals("preoccupied with a single leaf you will not see the tree preoccupied with a single tree you will miss the entire forest", MorseCodeConverter.convertToEnglish(file));
+		} catch (FileNotFoundException e) {
+			assertTrue("An unwanted exception was caught", false);
+		}
+	}
+	
+
+}
