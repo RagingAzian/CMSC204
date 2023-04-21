29df0872e0e40e522c2ef506f9d923f7bdaae296
Justin Nguyen <justinn123123123@gmail.com>
2023-04-21 13:43:19 -0400

    finished ass 6

--- Assignment6/src/TownGraphManager.java
+++ Assignment6/src/TownGraphManager.java
@@ -1,63 +1,83 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class TownGraphManager implements TownGraphManagerInterface {
-	private Graph graph = new Graph();
-	
+    private Graph graph = new Graph();
+
     public boolean addRoad(String town1, String town2, int weight, String roadName) {
-		return(graph.addEdge(new Town(town1), new Town(town2), weight, roadName) != null);		
-	}
+        return (graph.addEdge(new Town(town1), new Town(town2), weight, roadName) != null);
+    }
 
-	public String getRoad(String town1, String town2) {
-		return ((Road) graph.getEdge(new Town(town1), new Town(town2))).getName();
-	}
+    public String getRoad(String town1, String town2) {
+        return ((Road) graph.getEdge(new Town(town1), new Town(town2))).getName();
+    }
 
-	public boolean addTown(String v) {
-		return graph.addVertex(new Town(v));
-	}
+    public boolean addTown(String v) {
+        return graph.addVertex(new Town(v));
+    }
 
-	public Town getTown(String name) {
-		return new Town(name);
-	}
+    public Town getTown(String name) {
+        return new Town(name);
+    }
 
-	public boolean containsTown(String v) {
-		return graph.containsVertex(new Town(v));
-	}
+    public boolean containsTown(String v) {
+        return graph.containsVertex(new Town(v));
+    }
 
-	public boolean containsRoadConnection(String town1, String town2) {
-		return graph.containsEdge(new Town(town1), new Town(town2));
-	}
+    public boolean containsRoadConnection(String town1, String town2) {
+        return graph.containsEdge(new Town(town1), new Town(town2));
+    }
 
-	public ArrayList<String> allRoads() {
-		ArrayList<Road> roads = new ArrayList<Road>(graph.edgeSet());
-		ArrayList<String> roadsList = new ArrayList<String>();
-		for(Road r: roads) {
-			roadsList.add(r.getName());
-		}
-		Collections.sort(roadsList);
-		return roadsList;
-	}
+    public ArrayList<String> allRoads() {
+        ArrayList<Road> roads = new ArrayList<Road>(graph.edgeSet());
+        ArrayList<String> roadsList = new ArrayList<String>();
+        for (Road r : roads) {
+            roadsList.add(r.getName());
+        }
+        Collections.sort(roadsList);
+        return roadsList;
+    }
 
-	public boolean deleteRoadConnection(String town1, String town2, String road) {
-		return(graph.removeEdge(new Town(town1), new Town(town2), 0, road) != null);
-	}
+    public boolean deleteRoadConnection(String town1, String town2, String road) {
+        return (graph.removeEdge(new Town(town1), new Town(town2), 0, road) != null);
+    }
 
-	public boolean deleteTown(String v) {
-		return graph.removeVertex(new Town(v));
-	}
+    public boolean deleteTown(String v) {
+        return graph.removeVertex(new Town(v));
+    }
 
-	public ArrayList<String> allTowns() {
-		ArrayList<Town> towns = new ArrayList<>(graph.vertexSet());
-		ArrayList<String> townsList= new ArrayList<>();
-		for(Town town: towns) {
-			townsList.add(town.getName());
-		}
-		Collections.sort(townsList);
-		return townsList;
-	}
+    public ArrayList<String> allTowns() {
+        ArrayList<Town> towns = new ArrayList<>(graph.vertexSet());
+        ArrayList<String> townsList = new ArrayList<>();
+        for (Town town : towns) {
+            townsList.add(town.getName());
+        }
+        Collections.sort(townsList);
+        return townsList;
+    }
 
-	public ArrayList<String> getPath(String town1, String town2) {
-		return graph.shortestPath(new Town(town1), new Town(town2));
-	}
+    public ArrayList<String> getPath(String town1, String town2) {
+        return graph.shortestPath(new Town(town1), new Town(town2));
+    }
 
+    public void populateTownGraph(File file) throws FileNotFoundException{
+        Scanner scan = new Scanner(file);
+        while(scan.hasNextLine()){
+            String line = scan.nextLine();
+            String[] splitText = line.split(",");
+            String roadName = splitText[0];
+            splitText = splitText[1].split(";");
+            int weight = Integer.parseInt(splitText[0]);
+            splitText = line.split(";");
+            String town1 = splitText[1];
+            String town2 = splitText[2];
+            graph.addVertex(new Town(town1));
+            graph.addVertex(new Town(town2));
+            graph.addEdge(new Town(town1), new Town(town2), weight, roadName);
+        }
+        scan.close();
+    }
 }
